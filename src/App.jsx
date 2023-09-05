1dca30e7b8fdef1253b38e677c40b9d056683038
sween <khansween@gmail.com>
2023-09-05 23:52:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react' 
+import './App.css'
+
+function App() { 
+
+  return (
+    
+      <div className='app'>
+      <h2>how are you</h2>
+        </div> 
+  )
+}
+
+export default App
