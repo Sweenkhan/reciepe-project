3c0bc521fd700afb5d23bf7e4aa24d4e8dfaaebc
sween <khansween@gmail.com>
2023-09-10 23:59:43 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import React, { useState } from 'react' 
 import './App.css'
 import Reciepe from './assets/Reciepe/Reciepe'
+import { BrowserRouter, Router, Route } from 'react-router-dom'
+import Header from './assets/Header/Header'
+
 
 function App() { 
 
-  return (
-    
+  return ( 
       <div className='app'>
-      <h2>how are you</h2>
+    <BrowserRouter>
+      <Header />
+      <Router >
+        <Route  path="/" element={<Home />}></Route> 
+        <Route  path="/about" element={<About />}></Route> 
+        <Route  path="/reciepe" element={<Reciepe />}></Route> 
+        <Route  path="/category" element={<Category />}></Route> 
+
+      </Router>
+    </BrowserRouter>
       <Reciepe />
         </div> 
   )
