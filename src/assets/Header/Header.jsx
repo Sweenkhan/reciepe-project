92ba6c2f218d14549535a0473e4dd0af993f0222
sween <khansween@gmail.com>
2023-09-10 23:54:02 +0530

    added links in header

--- src/assets/Header/Header.jsx
+++ src/assets/Header/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import "./Header.css"
+import Link from "react-router-dom"
+
+
 
 function Header() {
   return (
-    <div className="header">This is Header</div>
+    <div className="header">
+      <h2>Meal</h2>
+      <ul className='headerUl'>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/category">Category</Link></li>
+        <li><Link to="/reciepe">Reciepe</Link></li>
+        <li><Link to="/Register">Sign-up</Link></li>
+      </ul>
+    </div>
   )
 }
 
