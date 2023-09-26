3e32e6d14c83d391f32fe0b98d3cd990f4124b30
sween <khansween@gmail.com>
2023-09-26 22:09:58 +0530

    c

--- src/assets/Reciepe/Reciepe.jsx
+++ src/assets/Reciepe/Reciepe.jsx
@@ -10,11 +10,11 @@ function Reciepe() {
   const [reciepes, setReciepes] = useState([])
  
 
-  //-------------------------------------------FETCHING DATA FROM API BY CATEGORY--------------------------//
 
+  //-------------------------------------------FETCHING DATA FROM API BY CATEGORY--------------------------// 
   useEffect(() => {
 
-    axios.get("http://localhost:8080/getAllBooks")
+    axios.get("http://localhost:8000/getAllCategory")
       .then((result) => { 
        console.log(result.data.message)
       })
@@ -26,14 +26,14 @@ function Reciepe() {
   return (
     <div className="reciepeCnt">
       <h2>This is the reciepe page.</h2> 
-      {
+      {/* {
         categories.map((categ, index) => {
           return <div key={index}>
             <img src={categ.strCategoryThumb} alt="main-img" />
             <h2>{categ.strCategory}</h2>
           </div>
         })
-      }
+      } */}
     </div>
 
   )
