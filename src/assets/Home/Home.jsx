efac87b8c056570fb462ea66e43578a1ec0180e3
sween <khansween@gmail.com>
2023-11-23 02:14:20 +0530

    commit of the day

--- src/assets/Home/Home.jsx
+++ src/assets/Home/Home.jsx
@@ -18,9 +18,9 @@ function Home() {
         
 
         <Typewriter />
-        {/* <div className='mainPageContent'> 
+        <div className='mainPageContent'> 
         <p>At RECIEPE, we believe that every meal is an opportunity for a delightful adventure. Whether you're a seasoned chef or a novice in the kitchen, our mission is to inspire your inner foodie and elevate your cooking experience. Explore a world of flavors, discover new techniques, and create unforgettable dishes .</p>
-        </div> */}
+        </div>
         <div className='mainPageList'> 
         <Allcategory />
         </div>
