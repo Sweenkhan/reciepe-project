b6104007de16345d827be8780fedbd608c1ab149
sween <khansween@gmail.com>
2023-11-24 01:45:04 +0530

    added mousenter func in search icon

--- src/assets/Header/Header.jsx
+++ src/assets/Header/Header.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
 // import logo from "../Images/loogo.png"
 import SearchIcon from "@mui/icons-material/Search";
 
 function Header() {
+
+  const [showsearchInput, setShowsearchInput] = useState(false)
+
+  console.log(showsearchInput)
+
+
+  function handlonMouseOver(){ 
+      setShowsearchInput(true)
+  }
+
+  function handlonMouseOut(){
+    setShowsearchInput(false)
+  }
+
   return (
     <div className="header">
       <h2>QRECIEPE</h2>
-      <div className="headerRight">
+      <div className="headerRight"  onMouseEnter={() => handlonMouseOver()} onMouseLeave={handlonMouseOut} >
+      <div className="formDiv"> 
         <form>
-          <input type="text" className="searctInput" />
-          <SearchIcon className="search"/>
+          <input type="text" className={(showsearchInput) ? "showInput" : "closeInput" } />
+          <span> <SearchIcon className="search"/></span>
         </form>
+        </div>
         <ul className="headerUl">
           <li>
             <Link to="/reciepe">Reciepes</Link>
