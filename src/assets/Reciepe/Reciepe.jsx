32e22827edcd0fad4eb7ed39173970a2c72c4150
sween <khansween@gmail.com>
2024-01-29 23:43:01 +0530

    modified data

--- src/assets/Reciepe/Reciepe.jsx
+++ src/assets/Reciepe/Reciepe.jsx
@@ -6,7 +6,7 @@ function Reciepe() {
   
   return (
      <div className='reciepeDiv' style={{paddingTop: "40px"}}>
-      <h1></h1>
+      <h1> Choose your ategory </h1>
       <Allcategory />
      </div>
 
